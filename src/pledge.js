e42d35280ae913bfd772a2dcf80171f84bf9b268
Keri Warr <keri@warr.ca>
2016-10-19 23:49:22 -0700

    Build out most other functions

--- src/pledge.js
+++ src/pledge.js
@@ -1,4 +1,6 @@
 import fetch from 'node-fetch';
+import _ from 'lodash';
+
 import logger from './logger';
 
 
@@ -8,9 +10,11 @@ const WAGERS_PATH = '/wagers';
 
 const ID_REGEX = /^\d+$/;
 const MESSAGE_REGEX = /^(?:I )?pledge (.+)$/i;
-// The second dash is actually a unicode double dash
+// The second option is actually a unicode double dash
 const OPTION_REGEX = /^(--|—)/;
 const CENTS_REGEX = /\.0+$/;
+// const TAG_REGEX = /^@[a-zA-Z0-9.-_]{1,21}$/;
+const USER_ID_REGEX = /^<@(U[A-Z0-9]+)>$/;
 
 const DEFAULT_CURRENCY = 'CAD';
 const CURRENCY_MAP = {
@@ -18,19 +22,31 @@ const CURRENCY_MAP = {
   USD: ':flag-us:',
 };
 
+const pastTenseify = (verb) => {
+  if (verb === 'take') return 'taken';
+  else if (verb === 'cancel') return 'cancelled';
+  return `${verb}ed`;
+};
+
 const ERRORS = {
+  operationFailure: kind => `Sorry, you can't ${kind} that wager.`,
   wagerNotFound: 'I couldn\'t find that wager.',
-  acceptFailure: 'Sorry, you can\'t accept that wager.',
   serverFailure: 'Sorry, the request failed.',
   missingIdArgument: 'You must specify an id.',
+  missingUserArgument: 'You must specify a @user',
+  nonExistentUser: 'That user is not in this team.',
+  noResults: kind => `I couldn't find any${kind ? ` ${kind}` : ''} wagers.`,
 };
 const MESSAGES = {
-  acceptSuccess: 'You\'ve accepted the wager!',
+  operationSuccess: kind => `You've ${pastTenseify(kind)} the wager!`,
 };
 
 const stripZeroCents = str => str && str.replace(CENTS_REGEX, '');
 
 const getIdFromStr = str => str && ((str.match(ID_REGEX) || [])[0] || null);
+
+const getUserIdFromStr = str =>
+  str && ((str.match(USER_ID_REGEX) || [])[1] || null);
 
 const italic = str => str && `_${str}_`;
 
@@ -51,7 +67,18 @@ const getOfferDescription = ({ description, amount, currency }) => {
     : `${bold(stripZeroCents(amount))}${currencyDisplay}`;
 };
 
-const getWagerDescription = (wager) => {
+const nameToTag = ({ name, userNameMap }) => {
+  const tag = (_.find(userNameMap, user => user.real_name === name) || {}).name;
+  return tag ? `@${tag}` : null;
+};
+
+const userIdToName = ({ id, userNameMap }) => {
+  const name = (_.find(userNameMap, user => user.id === id) || {}).real_name;
+  return name || null;
+};
+
+const baseWagerDescription = ({ showStatus = false } = {}, { userNameMap }) =>
+(wager) => {
   const id = wager.id;
   if (!id) return null;
   const makerOffer = getOfferDescription({
@@ -65,12 +92,21 @@ const getWagerDescription = (wager) => {
     currency: wager.taker_offer_currency,
   });
   const outcome = wager.outcome ? ` ~ ${wager.outcome}` : '';
-  const maker = wager.maker && wager.maker.split(' ')[0];
-  const taker = wager.taker && wager.taker.split(' ')[0];
+  const makerName = wager.maker && wager.maker.split(' ')[0];
+  const takerName = wager.taker && wager.taker.split(' ')[0];
+  const maker = nameToTag({ name: wager.maker, userNameMap }) || makerName;
+  const taker = nameToTag({ name: wager.taker, userNameMap }) || takerName;
+  const status = showStatus ? `-${italic(wager.status)}` : '';
 
-  return `${pre(id)}: ${italic(maker)}'s ${makerOffer} to ${italic(taker)}'s \
-${takerOffer}${outcome}\n`;
+  return `${pre(id)}${status}: ${maker}'s ${makerOffer} to ${taker}'s \
+${takerOffer}${outcome}`;
 };
+
+const getWagerDescription = baseWagerDescription.bind(null, {});
+
+const getWagerStatusDescription = baseWagerDescription.bind(null, {
+  showStatus: true,
+});
 
 const getInit = ({ data }) =>
 (data
@@ -108,8 +144,9 @@ const getWager = ({ id }) => fetchWrapper({
 const getWagers = ({ filters = [] } = {}) =>
   fetchWrapper({ url: `${API_ROOT}${WAGERS_PATH}` })
     .then(data => filters.reduce(
-      (wagers, filter) => wagers.filter(filter),
-      data)
+        (wagers, filter) => wagers.filter(filter),
+        data
+      )
     );
 
 const createOperation = operation =>
@@ -128,20 +165,78 @@ const requiresId = handler => (options) => {
   return handler({ id, ...options });
 };
 
-const handleAll = ({ sendReply }) =>
+const requiresUser = handler => (options) => {
+  const args = options.argString && options.argString.split(' ');
+  if (!args || args.length === 0 || !args[0]) {
+    return options.sendReply(ERRORS.missingUserArgument);
+  }
+  const usersName = userIdToName({
+    id: getUserIdFromStr(args[0]),
+    userNameMap: options.userNameMap,
+  });
+  if (!usersName) return options.sendReply(ERRORS.nonExistentUser);
+  return handler({ usersName, ...options });
+};
+
+const makeOperationHandler = ({ kind }) =>
+  requiresId(({ sendReply, id, fullName }) =>
+    createOperation({ kind, wager_id: id, user: fullName })
+      .then(() => sendReply(MESSAGES.operationSuccess(kind)))
+      .catch(() => sendReply(ERRORS.operationFailure(kind))));
+
+const makeShowStatusHandler = ({ status }) => ({ sendReply, userNameMap }) =>
+  getWagers({ filters: [(wager => wager.status === status)] })
+    .then(wagers => sendReply(
+      wagers
+        .map(getWagerDescription({ userNameMap }))
+        .join('\n')
+        || ERRORS.noResults(status)
+    )).catch(() => sendReply(ERRORS.serverFailure));
+
+const userInvoledInWager = ({ user }) => wager =>
+  wager.maker === user || wager.taker === user || wager.arbiter === user;
+
+const handleAll = ({ sendReply, userNameMap }) =>
   getWagers()
-    .then(wagers => sendReply(wagers.map(getWagerDescription).join('')))
-    .catch(() => sendReply(ERRORS.serverFailure));
+    .then(wagers => sendReply(
+      wagers.map(getWagerStatusDescription({ userNameMap })).join('\n')
+        || ERRORS.noResults()
+    )).catch(() => sendReply(ERRORS.serverFailure));
 
 const handleShow = requiresId(({ sendReply, id }) =>
   getWager({ id })
     .then(wager => sendReply(getWagerDescription(wager)))
     .catch(() => sendReply(ERRORS.wagerNotFound)));
 
-const handleAccept = requiresId(({ sendReply, id, fullName }) =>
-  createOperation({ kind: 'accept', wager_id: id, user: fullName })
-    .then(() => sendReply(MESSAGES.acceptSuccess))
-    .catch(() => sendReply(ERRORS.acceptFailure)));
+const handleAccept = makeOperationHandler({ kind: 'accept' });
+const handleReject = makeOperationHandler({ kind: 'reject' });
+const handleTake = makeOperationHandler({ kind: 'take' });
+const handleCancel = makeOperationHandler({ kind: 'cancel' });
+const handleClose = makeOperationHandler({ kind: 'close' });
+const handleAppeal = makeOperationHandler({ kind: 'appeal' });
+
+const handleListed = makeShowStatusHandler({ status: 'listed' });
+const handleUnaccepted = makeShowStatusHandler({ status: 'unaccepted' });
+const handleRejected = makeShowStatusHandler({ status: 'rejected' });
+const handleAccepted = makeShowStatusHandler({ status: 'accepted' });
+const handleClosed = makeShowStatusHandler({ status: 'closed' });
+const handleCompleted = makeShowStatusHandler({ status: 'completed' });
+const handleAppealed = makeShowStatusHandler({ status: 'appealed' });
+const handleCancelled = makeShowStatusHandler({ status: 'cancelled' });
+
+const handleMine = ({ sendReply, fullName, userNameMap }) =>
+  getWagers({ filters: [userInvoledInWager({ user: fullName })] })
+    .then(wagers => sendReply(
+      wagers.map(getWagerStatusDescription({ userNameMap })).join('\n')
+        || ERRORS.noResults('of your')
+    )).catch(() => sendReply(ERRORS.serverFailure));
+
+const handleUser = requiresUser(({ sendReply, usersName, userNameMap }) =>
+  getWagers({ filters: [userInvoledInWager({ user: usersName })] })
+    .then(wagers => sendReply(
+      wagers.map(getWagerStatusDescription({ userNameMap })).join('\n')
+        || ERRORS.noResults('of their')
+    )).catch(() => sendReply(ERRORS.serverFailure)));
 
 const makeSendReply = response => (reply) => {
   logger.info(`Sending response: ${reply}`);
@@ -149,7 +244,7 @@ const makeSendReply = response => (reply) => {
 };
 
 export default function pledge(message, users, response) {
-  const messageMatches = message.text.match(MESSAGE_REGEX);
+  const messageMatches = message.text && message.text.match(MESSAGE_REGEX);
   if (!messageMatches || messageMatches.length < 2) return;
   logger.info(`Received message: ${message.text}`);
 
@@ -158,75 +253,83 @@ export default function pledge(message, users, response) {
   const command = messageCommandArgs[0].replace(OPTION_REGEX, '');
   const argString = messageCommandArgs.slice(1).join(' ');
   const userId = message.user;
-  // const text = message.text;
   const user = users[userId];
   const fullName = user.real_name;
+  // const tag = user.name;
+  const userNameMap = _.values(users).map(
+    u => _.pick(u, ['name', 'real_name', 'id'])
+  );
+
+  const commandParams = { sendReply, argString, fullName, userNameMap };
 
   switch (command) {
     case '-w':
     case 'wagers':
     case 'all':
-      handleAll({ sendReply });
-      break;
-    case '-l':
-    case 'available':
-    case 'lited':
-
+      handleAll(commandParams);
       break;
     case '-s':
     case 'get':
     case 'show':
-      handleShow({ sendReply, argString });
+      handleShow(commandParams);
       break;
     case '-a':
     case 'accept':
-      handleAccept({ sendReply, argString, fullName });
+      handleAccept(commandParams);
       break;
     case '-r':
     case 'reject':
-
+      handleReject(commandParams);
       break;
     case '-t':
     case 'take':
-
+      handleTake(commandParams);
       break;
     case 'cancel':
-
+      handleCancel(commandParams);
       break;
     case '-c':
     case 'close':
-
+      handleClose(commandParams);
       break;
     case 'appeal':
-
+      handleAppeal(commandParams);
+      break;
+    case '-l':
+    case 'available':
+    case 'listed':
+      handleListed(commandParams);
       break;
     case 'unaccepted':
-
+      handleUnaccepted(commandParams);
       break;
     case 'rejected':
-
+      handleRejected(commandParams);
       break;
     case 'accepted':
     case 'open':
-
+      handleAccepted(commandParams);
       break;
     case 'closed':
-
+      handleClosed(commandParams);
       break;
     case 'completed':
-
+      handleCompleted(commandParams);
       break;
     case 'appealed':
-
+      handleAppealed(commandParams);
+      break;
+    case 'cancelled':
+      handleCancelled(commandParams);
       break;
     case '-m':
     case 'mine':
     case 'me':
-
+      handleMine(commandParams);
       break;
     case '-u':
     case 'user':
-
+      handleUser(commandParams);
       break;
     case '-h':
     case 'how':
